42e475c8addc7054b572c60d345369f848423169
KindaQuackDev <ryan.longstreth522@gmail.com>
2026-05-03 17:42:55 -0400

    removed KubeJS Folder

--- kubejs/server_scripts/shard_fixes.js
+++
@@ -1,9 +0,0 @@
-// server_scripts/geore_uranium_recipes.js
-
-ServerEvents.recipes(event => {
-  // Vanilla furnace smelting
-  event.smelting('oritech:raw_uranium', 'geore:uranium_shard')
-
-  // Vanilla blast furnace (Create bulk blasting is auto-generated from this)
-  event.blasting('oritech:raw_uranium', 'geore:uranium_shard')
-})
